d6f20eefd84fefdbc32fba2fde174a62e441250a
Naledi Kutta <vukutjhb025@student.wethinkcode.co.za>
2026-09-16 16:49:09 +0200

    fix:changed IngestionService app class layout for easier testing

--- ingestion-service/src/main/java/co/wethinkcode/healthsafe/IngestionServiceApp.java
+++ ingestion-service/src/main/java/co/wethinkcode/healthsafe/IngestionServiceApp.java
@@ -7,15 +7,36 @@ import java.util.ArrayList;
 
 public class IngestionServiceApp {
 
-    public static void main(String[] args) throws Exception {
-        Javalin app = Javalin.create().start(7030);
-        String filePath = "wards-outdated.csv";
-        ArrayList<WardRecordModel> cleaned = CsvCleaner.getCleanedWards();
+    private final Javalin server;
+    ArrayList<WardRecordModel> cleanedRecords = CsvCleaner.getCleanedWards();
 
-        app.get("/health", ctx -> ctx.result("OK"));
-            app.get("/wards", ctx -> {
-                ctx.json(cleaned);
-        });
+    public IngestionServiceApp() throws Exception {
+    this.server = Javalin.create();
+    this.server.get("/health", ctx -> ctx.result("OK"));
+    this.server.get("/wards",ctx -> ctx.json(cleanedRecords));
+
+    }
+
+    public Javalin start(){
+        return this.server.start(7030);
+    }
+
+    public Javalin stop(){
+        return this.server.stop();
+    }
+
+    public static void main(String[] args) throws Exception {
+        IngestionServiceApp api = new IngestionServiceApp();
+        api.start();
+
+//        Javalin app = Javalin.create().start(7030);
+//        String filePath = "wards-outdated.csv";
+//        ArrayList<WardRecordModel> cleaned = CsvCleaner.getCleanedWards();
+//
+//        app.get("/health", ctx -> ctx.result("OK"));
+//            app.get("/wards", ctx -> {
+//                ctx.json(cleaned);
+//        });
 
         // TODO: read and clean src/main/resources/wards-outdated.csv (wards, wings, specialist departments data —
         // trim whitespace, fix casing, normalize dates/booleans) and expose the
