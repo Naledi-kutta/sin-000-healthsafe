8d5f5d3c91fdb6cc1d60a063a4fe8b5bbf6321bc
Naledi Kutta <vukutjhb025@student.wethinkcode.co.za>
2026-09-16 17:03:37 +0200

    fix:changed layout of Ward Service class for easier testing

--- ward-service/src/main/java/co/wethinkcode/healthsafe/WardServiceApp.java
+++ ward-service/src/main/java/co/wethinkcode/healthsafe/WardServiceApp.java
@@ -1,13 +1,48 @@
 package co.wethinkcode.healthsafe;
 
 import io.javalin.Javalin;
+import kong.unirest.Unirest;
+import kong.unirest.HttpResponse;
+//import java.net.http.HttpResponse;
+import java.rmi.server.UnicastRemoteObject;
 
 public class WardServiceApp {
+    private static final String INGESTION_SERVICE_URL = "http://localhost:7030/wards";
+    private final Javalin server;
+    //object where data from Ingestion will be populated
+
+    public WardServiceApp(){
+        this.server = Javalin.create();
+        this.server.get("/wards",ctx -> ctx.json(getWardsFromIngestionService()));
+        //this.server.get("/wards",ctx -> ctx.json("Wards is working"));
+
+    }
+
+    public Javalin start(){
+        return this.server.start(7031);
+    }
+
+    public Javalin stop(){
+        return this.server.stop();
+    }
+
+
+    public String getWardsFromIngestionService(){
+        //calls Ingestion Service's /wards endpoint and returns a response
+        HttpResponse<String> response = Unirest.get("http://localhost:7030/wards")
+                .asString();
+
+        return response.getBody();
+    }
+
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7031);
+        WardServiceApp api = new WardServiceApp();
+        api.start();
 
-        app.get("/health", ctx -> ctx.result("OK"));
+//        Javalin app = Javalin.create().start(7031);
+//        app.get("/health", ctx -> ctx.result("OK"));
+
 
         // TODO (Provides lists of wards and departments.)
         // Add domain endpoints for ward-service here.
