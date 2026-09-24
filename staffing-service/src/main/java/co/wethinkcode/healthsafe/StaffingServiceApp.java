1956befe87601877c43b3f1ff5f4ab5916eee33e
Naledi Kutta <vukutjhb025@student.wethinkcode.co.za>
2026-09-24 13:32:40 +0200

    feat:Added /staffing endpoint to StaffingServiceApp

--- staffing-service/src/main/java/co/wethinkcode/healthsafe/StaffingServiceApp.java
+++ staffing-service/src/main/java/co/wethinkcode/healthsafe/StaffingServiceApp.java
@@ -3,11 +3,36 @@ package co.wethinkcode.healthsafe;
 import io.javalin.Javalin;
 
 public class StaffingServiceApp {
+    private final Javalin server;
+    private final StaffingService staffingService;
+
+
+    public StaffingServiceApp(){
+        this.staffingService = new StaffingService();
+        this.server = Javalin.create();
+        this.server.get("/health", ctx -> ctx.result("OK"));
+        this.server.get("/staffing/{id}",ctx -> {
+            StaffingResponse response = staffingService.getStaffingForWard(
+                    ctx.pathParam("id")
+            );
+            ctx.json(response);
+                });
+    }
+
+    public Javalin start(){
+       return this.server.start(7033);
+    }
+
+    public Javalin stop(){
+        return this.server.stop();
+    }
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7033);
+        StaffingServiceApp app = new StaffingServiceApp();
+        app.start();
+//        Javalin app = Javalin.create().start(7033);
 
-        app.get("/health", ctx -> ctx.result("OK"));
+        //app.get("/health", ctx -> ctx.result("OK"));
 
         // TODO (Provides on-call schedules for doctors based on ward and status.)
         // Add domain endpoints for staffing-service here.
