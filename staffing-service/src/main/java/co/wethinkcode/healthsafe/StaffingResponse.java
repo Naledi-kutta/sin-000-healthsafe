a4bd181117692cc0a2ce11f492f09103b9619208
Naledi Kutta <vukutjhb025@student.wethinkcode.co.za>
2026-09-24 13:39:31 +0200

    refactor:Added Staffing Response class to handle response for staffing schedule

---
+++ staffing-service/src/main/java/co/wethinkcode/healthsafe/StaffingResponse.java
@@ -0,0 +1,42 @@
+package co.wethinkcode.healthsafe;
+//This class is used for returning json strings and parsing them into java objects
+public class StaffingResponse {
+    private int alertLevel;
+    private String wardId;
+    private String department;
+
+    public StaffingResponse(){};
+
+    public StaffingResponse(String wardId, int alertLevel) {
+        this.wardId = wardId;
+        this.alertLevel = alertLevel;
+    }
+
+
+    public StaffingResponse(int level, String wardId, String department){
+        this.alertLevel = level;
+        this.wardId = wardId;
+        this.department = department;
+    }
+//
+//    public StaffingResponse(String ward){
+//        this.wardId = ward;
+//    }
+//
+//    public StaffingResponse(int level){
+//        this.alertLevel = level;
+//    }
+//
+
+    public int getAlertLevel() {
+        return alertLevel;
+    }
+
+    public String getWardId() {
+        return wardId;
+    }
+
+    public String getDepartment(){
+       return getDepartment();
+    }
+}
