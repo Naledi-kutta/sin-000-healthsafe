ac9325f219fd5134451af4cca0f72fef60c7c7d6
Naledi Kutta <vukutjhb025@student.wethinkcode.co.za>
2026-09-24 13:45:55 +0200

    refactor:Added ObjectMapper import

--- ward-service/src/main/java/co/wethinkcode/healthsafe/WardService.java
+++ ward-service/src/main/java/co/wethinkcode/healthsafe/WardService.java
@@ -1,29 +1,23 @@
 package co.wethinkcode.healthsafe;
 
 import kong.unirest.HttpResponse;
-import kong.unirest.ObjectMapper;
 import kong.unirest.Unirest;
-import kong.unirest.UnirestException;
-import com.fasterxml.jackson.databind.ObjectMapper.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
-import java.nio.channels.NetworkChannel;
 import java.util.ArrayList;
-import java.util.HashMap;
 
-//returns wardIds and departments for WardServiceApp to ingest
 public class WardService {
     private static final String INGESTION_SERVICE_URL = "http://localhost:7030/wards";
-    private final com.fasterxml.jackson.databind.ObjectMapper objectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public String getWardsFromIngestionService() {
-        //calls Ingestion Service's /wards endpoint and returns wards
         HttpResponse<String> response = null;
         try {
             response = Unirest.get(INGESTION_SERVICE_URL)
                     .asString();
             return response.getBody();
-            //if Ingestion service is offline, output error
+
 
         } catch (Exception error) {
             throw new RuntimeException("Ingestion Service is unavailable.");
@@ -31,29 +25,21 @@ public class WardService {
 
     }
 
-    //method that lists a specific ward when given a ward ID
-    public ArrayList<WardResponse> getWardByWardId(String wardId) {
-        //access WardRecordModel
-        ArrayList<WardResponse> matchingWards = new ArrayList<>();
+
+    public ArrayList<WardServiceResponse> getWardByWardId(String wardId) {
+        ArrayList<WardServiceResponse> matchingWards = new ArrayList<>();
 
         try {
-            //convert response from ingestion service into java objects(WardRecordModel)
             HttpResponse<String> response = null;
             response = Unirest.get(INGESTION_SERVICE_URL)
                     .asString();
 
-//            HttpResponse<String> jsonString = response;
-//            WardResponse[] wards = objectMapper.readValue(
-//                    jsonString.getBody(),
-//                    WardResponse[].class
-//            );
-
-            WardResponse[] wards = objectMapper.readValue(
+            WardServiceResponse[] wards = objectMapper.readValue(
                     response.getBody(),
-                    WardResponse[].class
+                    WardServiceResponse[].class
             );
 
-            for (WardResponse ward : wards) {
+            for (WardServiceResponse ward : wards) {
                 if (ward.getWardId().equalsIgnoreCase(wardId)) {
                     matchingWards.add(ward);
                     break;
