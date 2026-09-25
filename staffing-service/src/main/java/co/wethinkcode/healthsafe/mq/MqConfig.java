aa05fe59bfba2b787669803a158e4bc5982ca1de
Naledi Kutta <vukutjhb025@student.wethinkcode.co.za>
2026-09-25 12:01:55 +0200

    feat:Implemented MqConfig in Staffing Service

--- staffing-service/src/main/java/co/wethinkcode/healthsafe/mq/MqConfig.java
+++ staffing-service/src/main/java/co/wethinkcode/healthsafe/mq/MqConfig.java
@@ -1,5 +1,9 @@
 package co.wethinkcode.healthsafe.mq;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 /**
  * Shared by every producer/consumer service that talks to the "staffing-events-topic"
  * ActiveMQ topic. Duplicated into each participating service's own source tree,
@@ -10,6 +14,33 @@ public final class MqConfig {
     public static final String BROKER_URL = "tcp://localhost:61616";
     public static final String TOPIC = "staffing-events-topic";
 
-    private MqConfig() {
+    public MqConfig() {
     }
+
+    public void sendMessage(String wardId,int alertLevel) {
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(BROKER_URL);
+
+            try (
+                    Connection connection = connectionFactory.createConnection()) {
+                connection.start();
+
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                Destination destination = session.createTopic(TOPIC);
+
+                MessageProducer producer = session.createProducer(destination);
+                producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+
+                String payload = String.format( "{\"wardId\":\"%s\",\"alertLevel\":%d}", wardId, alertLevel);
+
+                TextMessage message = session.createTextMessage(payload);
+                producer.send(message);
+
+                System.out.println("Sent messsage: " + message.getText());
+
+            } catch (JMSException error) {
+                error.printStackTrace();
+            }
+        }
 }
+
